e7c53e58c7e4468b3f13556e3f6d7170b3bcc23f
Moises Velasquez <moises007@gmail.com>
2026-01-20 18:04:38 -0500

    fix register-user dto

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -13,7 +13,7 @@ export class RegisterUserDto {
 
     if (!name) return ["Missing name"];
     if (!email) return ["Missing email"];
-    if (regularExps.email.test(email)) return ["Email is not valid"];
+    if (!regularExps.email.test(email)) return ["Email is not valid"];
     if (!password) return ["Missing password"];
     if (password.length < 6) return ["Password too short"];
 

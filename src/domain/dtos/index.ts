41ec4785528232db26999b8dd0553e4b043b0d63
Moises Velasquez <moises007@gmail.com>
2026-01-22 23:33:45 -0500

    add PaginationDto

--- src/domain/dtos/index.ts
+++ src/domain/dtos/index.ts
@@ -1,3 +1,4 @@
 export { CreateCategoryDto } from "./catergory/create-category.dto";
 export { LoginUserDto } from "./auth/login-user.dto";
+export { PaginationDto } from "./shared/pagination.dto";
 export { RegisterUserDto } from "./auth/register-user.dto";
--- src/presentation/category/category.controller.ts
+++ src/presentation/category/category.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CreateCategoryDto, CustomError } from "../../domain";
+import { CreateCategoryDto, CustomError, PaginationDto } from "../../domain";
 import { CategoryService } from "../services";
 
 export class CategoryController {
@@ -25,10 +25,15 @@ export class CategoryController {
   };
 
   getCategories = (req: Request, res: Response) => {
-    this.categoryService
-      .getCategories()
-      .then((catergories) => res.status(200).json(catergories))
-      .catch((error) => this.handleError(error, res));
-    // res.json("Get Categories");
+    const { page = 1, limit = 10 } = req.query;
+    const [error, paginationDto] = PaginationDto.create(+page, +limit);
+
+    if (error) return res.status(400).json({ error });
+
+    res.json(paginationDto);
+    // this.categoryService
+    //   .getCategories()
+    //   .then((catergories) => res.status(200).json(catergories))
+    //   .catch((error) => this.handleError(error, res));
   };
 }
